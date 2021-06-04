8599b1e43fcb1ee581deb9e3585c30893c8fa74e
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-06-04 17:32:54 -0400

    Adds installation script for maxquant.

---
+++ maxquant/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$MAXQUANT" ]
+then
+  echo "MAXQUANT environment variable must be defined, please load a 'maxquant' module"
+  exit 1
+fi
+
+
+# Download maxquant.
+if [ ! -d "$MAXQUANT" ]
+then
+  echo "Creating folder $MAXQUANT"
+  mkdir -p "$MAXQUANT"
+fi
+cd "$MAXQUANT" || { echo "Folder $MAXQUANT does not exists"; exit 1; }
+FILENAME=MaxQuant-"$MAXQUANT_VERSION".zip
+wget https://datahub-490-pl6.p.genap.ca/apps/maxquant/"$FILENAME"
+unzip "$FILENAME"
+mv MaxQuant/* .
+rm "$FILENAME"
