c507dfb443e7b455cf65c13cafa54fd78372783e
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-06-07 11:09:03 -0400

    Removes useless pip uninstall command in maxquant-tools install.sh.

--- maxquant-tools/install.sh
+++ maxquant-tools/install.sh
@@ -30,6 +30,5 @@ echo "Creating python virtual environment at $VENV"
 python3 -m venv "$VENV"
 VERSION=$(git --git-dir="$MAXQUANT_TOOLS"/.git rev-parse --abbrev-ref HEAD)
 echo "Updating python libraries using $VERSION"
-"$VENV"/bin/pip uninstall -y MAXQUANT_TOOLS
 "$VENV"/bin/pip install git+file://"$MAXQUANT_TOOLS"@"$VERSION"
 find "$VENV" -type f -perm 750 -exec sed -i "1 s|^.*$|#!/usr/bin/env python3|g" {} \;
