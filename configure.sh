50ae43a2502de90fca4bc691e1f85bde988f879e
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-06-11 10:05:20 -0400

    Use project symbolic link for module folder in configure.sh.

--- configure.sh
+++ configure.sh
@@ -43,12 +43,29 @@ then
   }  >> ~/.bash_profile
 fi
 
+# Get path to modules.
+if [ -d "$HOME"/projects ]
+then
+  SCRIPT_PATH=$(dirname $(readlink -f "$0"))
+  for project in "$HOME"/projects/*
+  do
+    PROJECT_PATH=$(readlink -f "$project")
+    if [[ $SCRIPT_PATH == $PROJECT_PATH/* ]]
+    then
+      MODULES_BASE=$project/${SCRIPT_PATH:${#PROJECT_PATH}+1}
+    fi
+  done
+fi
+if [ -z "$MODULES_BASE" ]
+then
+  MODULES_BASE=$(dirname $(readlink -f "$0"))
+fi
+
 # Create .coulombelab-apps-addons file to allow loading of coulombe modules.
 if [ -f ~/.coulombelab-apps-addons ]
 then
   rm ~/.coulombelab-apps-addons
 fi
-MODULES_BASE=$(dirname $(readlink -f $0))
 {
   echo "## Add Coulombe Lab Modules scripts to PATH ##"
   echo "PATH=$MODULES_BASE:\$PATH"
