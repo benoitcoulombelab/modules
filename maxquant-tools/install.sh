2b6676e7319c16186559d959fcacc6374e7fdefd
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-08-10 10:19:23 -0400

    Fixes shebang for maxquant-tools when absolute path to project changes depending on user.

--- maxquant-tools/install.sh
+++ maxquant-tools/install.sh
@@ -31,5 +31,16 @@ python3 -m venv "$VENV"
 VERSION=$(git --git-dir="$MAXQUANT_TOOLS"/.git rev-parse --abbrev-ref HEAD)
 echo "Updating python libraries using $VERSION"
 "$VENV"/bin/pip install git+file://"$MAXQUANT_TOOLS"@"$VERSION"
-VENV_REAL=$(readlink -f "$VENV")
-find "$VENV" -type f -perm 750 -exec sed -i "1 s|^.*$|#!$VENV_REAL/bin/python3|g" {} \;
+
+# Fix shebang for python files.
+find "$VENV/bin" -type f -executable -exec sed -i "1 s|^#\!.*$|#!/usr/bin/env maxquanttools_python_wrapper.sh|g" {} \;
+if [ -f "$VENV"/bin/maxquanttools_python_wrapper.sh ]
+then
+  rm "$VENV"/bin/maxquanttools_python_wrapper.sh
+fi
+{
+  echo '#!/bin/bash'
+  echo 'python="$MAXQUANT_TOOLS"/venv/bin/python3'
+  echo 'exec "$python" "$@"'
+} >> "$VENV"/bin/maxquanttools_python_wrapper.sh
+chmod 755 "$VENV"/bin/maxquanttools_python_wrapper.sh
