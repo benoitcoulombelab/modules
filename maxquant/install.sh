5e43df8d49ad663dc1658664e44cf4f9de8dc77f
Christian Poitras <christian.poitras@ircm.qc.ca>
2022-02-23 09:33:01 -0500

    Fixes MaxQuant installation due to GenAP certificate error.

--- maxquant/install.sh
+++ maxquant/install.sh
@@ -30,7 +30,7 @@ clean_module_dir "$MAXQUANT"
 echo "Installing MaxQuant in folder $MAXQUANT"
 cd "$MAXQUANT" || { echo "Folder $MAXQUANT does not exists"; exit 1; }
 FILENAME=MaxQuant-"$MAXQUANT_VERSION".zip
-wget https://datahub-490-pl6.p.genap.ca/apps/maxquant/"$FILENAME"
+wget --no-check-certificate https://datahub-490-pl6.p.genap.ca/apps/maxquant/"$FILENAME"
 unzip "$FILENAME"
 mv MaxQuant/* .
 rmdir MaxQuant
