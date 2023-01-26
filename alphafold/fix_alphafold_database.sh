8a5508c54b383e4f0f946e1e610cc0f92b9cd3dc
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-01-26 09:57:46 -0500

    Load bioawk module on compute canada for fix_alphafold_database.sh script.

--- alphafold/fix_alphafold_database.sh
+++ alphafold/fix_alphafold_database.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -n "$CC_CLUSTER" ]
+then
+  module purge
+  module load StdEnv/2020 bioawk/1.0
+fi
+
 echo "Backing up pdb_seqres.txt to pdb_seqres_bak.txt"
 cp data/pdb_seqres/pdb_seqres.txt data/pdb_seqres/pdb_seqres_bak.txt
 echo "Removing incorrect sequences from pdb_seqres.txt"
