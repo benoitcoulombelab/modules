d0457579ecaf7ab5fba241a59716b55fed225d07
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-03-24 10:06:04 -0400

    Rename aphafold-interactions to DeepProteinConnector.

--- deepproteinconnector/1.0.lua
+++ deepproteinconnector/1.0.lua
@@ -1,13 +1,13 @@
 help([[
 For detailed instructions, go to:
-    https://github.com/benoitcoulombelab/alphafold-interactions
+    https://github.com/benoitcoulombelab/DeepProteinConnector
 
 This module sets the following environment variables:
-    ALPHAFOLD_INTERACTIONS:  directory containing alphafold-interactions
+    DEEPPROTEINCONNECTOR:  directory containing DeepProteinConnector
 ]])
 
 whatis("Version: 1.0")
-whatis("URL: https://github.com/benoitcoulombelab/alphafold-interactions")
+whatis("URL: https://github.com/benoitcoulombelab/DeepProteinConnector")
 whatis("Description: This software tries to guess which proteins interact with a protein of interest using AlphaFold multimer")
 
 local module_path = myFileName()
@@ -16,5 +16,5 @@ local module_base = module_path:sub(1,module_path:find(module_name,1,true)-2)
 local apps_base = module_base:gsub("(.*)/(.*)","%1")
 local home = pathJoin(apps_base, module_name)
 prepend_path("PATH", pathJoin(home, "venv/bin"))
-setenv("ALPHAFOLD_INTERACTIONS", home)
-setenv("ALPHAFOLD_INTERACTIONS_VERSION", myModuleVersion())
+setenv("DEEPPROTEINCONNECTOR", home)
+setenv("DEEPPROTEINCONNECTOR_VERSION", myModuleVersion())
