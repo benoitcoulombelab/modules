c5ae3a184f369f5ee5970166f764e2f27f86769c
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-05-20 23:03:44 -0400

    Fixes issue of patch not applied in alphafold install.sh script.

--- alphafold/install.sh
+++ alphafold/install.sh
@@ -4,7 +4,7 @@
 set -e
 
 # cd to script directory
-script_path=$(dirname "$0")
+script_path=$(dirname $(readlink -f "$0"))
 cd "$script_path" || { echo "Folder $script_path does not exists"; exit 1; }
 
 # Commons functions
@@ -21,20 +21,21 @@ then
 else
   module load StdEnv/2020 python/3.9.6 singularity/3.8 alphafold/"$version"
 fi
+version="${ALPHAFOLD_VERSION}"
 
 
 clean_module_dir "$ALPHAFOLD"
 echo "Installing alphafold in folder $ALPHAFOLD"
 cd "$ALPHAFOLD" || { echo "Folder $ALPHAFOLD does not exists"; exit 1; }
 git clone https://github.com/dialvarezs/alphafold.git .
+git checkout "v${version}"
 patch="${script_path}/alphafold-${version}.patch"
-git checkout "v${ALPHAFOLD_VERSION}"
 if [ -f "${patch}" ]
 then
   echo "Applying patch file ${patch}"
   git apply "${patch}"
 fi
-git clone --branch "v${ALPHAFOLD_VERSION}" https://github.com/prehensilecode/alphafold_singularity singularity
+git clone --branch "v${version}" https://github.com/prehensilecode/alphafold_singularity singularity
 pushd singularity
 patch="${script_path}/alphafold_singularity-${version}.patch"
 if [ -f "${patch}" ]
@@ -53,7 +54,7 @@ python3 -m venv "$venv"
 "$venv/bin/pip" install -r singularity/requirements.txt
 
 # Install singularity container
-if ! wget -nv -O alphafold.sif "https://datahub-490-pl6.p.genap.ca/apps/alphafold/alphafold-${ALPHAFOLD_VERSION}.sif"
+if ! wget -nv -O alphafold.sif "https://datahub-490-pl6.p.genap.ca/apps/alphafold/alphafold-${version}.sif"
 then
   echo "AlphaFold singularity container does not exists for MaxQuant version $version"
 fi
