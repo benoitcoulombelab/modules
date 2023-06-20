368561d925abeec56f5c310a03f5fc5d6f1c1fbd
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-06-20 15:38:58 -0400

    Removes unnecessary python requirement for maxquant-tools modules.

--- maxquant-tools/1.0.lua
+++ maxquant-tools/1.0.lua
@@ -8,8 +8,6 @@ whatis("Keywords: MaxQuant, Utility")
 whatis("URL: https://github.com/benoitcoulombelab/maxquant-tools")
 whatis("Description: Utilities to run MaxQuant on Compute Canada server.")
 
-prereq(atleast("python","3.5.4"))
-
 local module_path = myFileName()
 local module_name = myModuleFullName()
 local module_base = module_path:sub(1,module_path:find(module_name,1,true)-2)
