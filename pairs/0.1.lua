a0829e4625b5ccc2e64e38bb939a760e039abb86
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-10-16 18:47:36 -0400

    Adds pairs module version 0.1.

---
+++ pairs/0.1.lua
@@ -0,0 +1,20 @@
+help([[
+For detailed instructions, go to:
+    https://github.com/benoitcoulombelab/pairs
+
+This module sets the following environment variables:
+    PAIRS:  directory containing pairs
+]])
+
+whatis("Version: 0.1")
+whatis("URL: https://github.com/benoitcoulombelab/pairs")
+whatis("Description: This software tries to guess which proteins interact with a protein of interest using AlphaFold-multimer")
+
+local module_path = myFileName()
+local module_name = myModuleFullName()
+local module_base = module_path:sub(1,module_path:find(module_name,1,true)-2)
+local apps_base = module_base:gsub("(.*)/(.*)","%1")
+local home = pathJoin(apps_base, module_name)
+prepend_path("PATH", pathJoin(home, "venv/bin"))
+setenv("PAIRS", home)
+setenv("PAIRS_VERSION", myModuleVersion())
